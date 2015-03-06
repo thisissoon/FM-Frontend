1fa8f90886b958efa3fa5de5d357699deacc0497
Edward Opare-Aryee <ed@thisissoon.com>
2015-03-06 11:10:06 +0000

    basic interface for thisissoon fm player

---
+++ app/js/fm-player/app.js
@@ -0,0 +1,10 @@
+"use strict";
+/**
+ * Main player module with allows the user to search tracks and view
+ * the current playlist
+ * @module   sn.fm.player
+ * @main     sn.fm.player
+ * @author   SOON_
+ * @requires ngRoute {@link https://docs.angularjs.org/api/ngRoute}
+ */
+angular.module("sn.fm.player", ["ngRoute"]);
