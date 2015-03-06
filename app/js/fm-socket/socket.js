a7391a5fa849ebc04576dfd636f9977b0bcdf70c
James Warren <mail@jamesjwarren.com>
2015-03-06 10:18:28 +0000

    fix jshint errors

--- app/js/fm-socket/socket.js
+++ app/js/fm-socket/socket.js
@@ -1,10 +1,12 @@
 "use strict";
 /**
  * Creates a socket instance for interacting with thisisoon.fm
- * Event handles can be passed to fmSocket using 'fmSocket.addListener("fm:player:play", eventHandler)'
- * the service will call the event handler with the arguments (data) every time a matching socket event
- * is received. `data` is a data object passed with the event, this will contain event and track data.
- * The handlers can be removed by using 'fmSocket.removeListener("fm:player:play", eventHandler)'
+ * Event handles can be passed to fmSocket using
+ * 'fmSocket.addListener("fm:player:play", eventHandler)' the service will
+ * call the event handler with the arguments (data) every time a matching
+ * socket event is received. `data` is a data object passed with the event,
+ * this will contain event and track data. The handlers can be removed by
+ * using 'fmSocket.removeListener("fm:player:play", eventHandler)'
  *
  * @module sn.fm.sockets
  * @author SOON_
@@ -28,15 +30,24 @@ angular.module("sn.fm.sockets", [
         "fm:player:add"
     ])
 
+    /**
+     * socket.io library
+     * for realtime event based communication
+     */
+    .value("io", window.io)
+
     .factory("fmSocket", [
+        "io",
         "socketFactory",
         "FM_SOCKET_ADDRESS",
         /**
          * @constructor
-         * @param   {Factory} socketFactory  factory for creating socket instances
+         * @param   {Object}  socket.io         socket transport library
+         * @param   {Factory} socketFactory     socket instance factory
+         * @param   {String}  FM_SOCKET_ADDRESS address of socket server
          * @returns {Object}  socket.io instance
          */
-        function (socketFactory, FM_SOCKET_ADDRESS) {
+        function (io, socketFactory, FM_SOCKET_ADDRESS) {
 
             return socketFactory({
                 prefix: "",
