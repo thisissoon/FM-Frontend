c00d8cff5c9547952189d0137351fb8c0db86084
Edward Opare-Aryee <ed@thisissoon.com>
2015-03-10 14:06:16 +0000

    fix sidebar toggle on mobile

--- app/js/fm-player/app.js
+++ app/js/fm-player/app.js
@@ -6,8 +6,25 @@
  * @module   sn.fm.player
  * @main     sn.fm.player
  * @author   SOON_
- * @requires ngRoute   {@link https://docs.angularjs.org/api/ngRoute}
- * @requires spotify   {@link https://github.com/eddiemoore/angular-spotify}
+ * @requires ngRoute    {@link https://docs.angularjs.org/api/ngRoute}
+ * @requires ngMaterial {@link https://material.angularjs.org/}
+ * @requires spotify    {@link https://github.com/eddiemoore/angular-spotify}
  * @requires sn.fm.api
  */
-angular.module("sn.fm.player", ["ngRoute", "spotify", "sn.fm.api"]);
+angular.module("sn.fm.player", ["ngRoute", "ngMaterial", "spotify", "sn.fm.api"])
+
+.run([
+    "$rootScope",
+    "$mdSidenav",
+    function ($rootScope, $mdSidenav){
+
+        /**
+         * Toggles the state of the sidebar
+         * @method toggleSidenav
+         * @param {String} menuId
+         */
+        $rootScope.toggleSidenav = function toggleSidenav(menuId){
+            $mdSidenav(menuId).toggle();
+        };
+    }
+]);
