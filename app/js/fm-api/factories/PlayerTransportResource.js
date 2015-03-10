0bb4500e239f02629bdb8657df10c613ff33c70c
James Warren <mail@jamesjwarren.com>
2015-03-10 12:10:32 +0000

    update transport resource to use player/current endpoint

--- app/js/fm-api/factories/PlayerTransportResource.js
+++ app/js/fm-api/factories/PlayerTransportResource.js
@@ -2,7 +2,7 @@
 /**
  * Factory which provides methods to perform on thisisoon FM API
  * transport endpoint. Which controls the playback of the current song
- * @class PlayerPlaylistResource
+ * @class PlayerTransportResource
  */
 angular.module("sn.fm.api").factory("PlayerTransportResource", [
     "$resource",
@@ -15,15 +15,14 @@ angular.module("sn.fm.api").factory("PlayerTransportResource", [
     function ($resource, SERVER_ADDRESS) {
 
         return $resource(
-            SERVER_ADDRESS + "player/",
+            SERVER_ADDRESS + "player/current",
             // Default values for url parameters.
             {},
             // Hash with declaration of custom action that should
             // extend the default set of resource actions
             {
                 track: {
-                    method: "GET",
-                    url: SERVER_ADDRESS + "player/playing"
+                    method: "GET"
                 },
 
                 pause: {
