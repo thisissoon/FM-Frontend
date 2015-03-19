07e291e5bab6f821462bee9cea036a101828fd6b
Edward Opare-Aryee <ed@thisissoon.com>
2015-03-19 12:19:08 +0000

    update material design black theme

--- app/js/fm-player/config.js
+++ app/js/fm-player/config.js
@@ -25,29 +25,31 @@ angular.module("sn.fm.player").config([
         }
 
         $mdThemingProvider.definePalette("black", {
-                "50": "000000",
-                "100": "000000",
-                "200": "000000",
-                "300": "000000",
-                "400": "0c0c0c",
-                "500": "191919",
-                "600": "262626",
-                "700": "323232",
-                "800": "3f3f3f",
-                "900": "4c4c4c",
-                "A100": "5d5d5d",
-                "A200": "6f6f6f",
-                "A400": "818181",
-                "A700": "939393",
-                "contrastDefaultColor": "light",    // whether, by default, text (contrast)
-                                                    // on this palette should be dark or light
-                "contrastDarkColors": ["50", "100", //hues which contrast should be "dark" by default
-                 "200", "300", "400", "A100"],
-                "contrastLightColors": undefined    // could also specify this if default was 'dark'
+                "50": "d3d3d3",
+                "100": "a8a8a8",
+                "200": "7c7c7c",
+                "300": "515151",
+                "400": "262626",
+                "500": "222222",
+                "600": "1e1e1e",
+                "700": "1a1a1a",
+                "800": "161616",
+                "900": "131313",
+                "A100": "222222",
+                "A200": "4e4e4e",
+                "A400": "7a7a7a",
+                "A700": "a6a6a6",
+                "contrastDefaultColor": "light"
              });
 
         $mdThemingProvider.theme("default")
-            .primaryPalette("black");
+            .primaryPalette("black", {
+                "default": "400",
+                "hue-1": "100",
+                "hue-2": "600",
+                "hue-3": "A100"
+            })
+            .accentPalette("yellow");
 
         $routeProvider
             .when("/", {
