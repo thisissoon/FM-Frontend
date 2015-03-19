71f8e0291ef6d9c009a7538c8d7f5d99a778e7b5
Edward Opare-Aryee <ed@thisissoon.com>
2015-03-19 12:18:49 +0000

    Add loading screen in-between route changes

---
+++ app/js/fm-loading-screen/loadingScreen.js
@@ -0,0 +1,53 @@
+"use strict";
+/**
+ * Implements a loading screen inbetween route changes
+ * @module sn.fm.loadingScreen
+ * @author SOON_
+ */
+angular.module("sn.fm.loadingScreen", [])
+/**
+ * @constant
+ * @property EVENTS
+ * @type {Object}
+ */
+.constant("EVENTS", {
+    ROUTE_CHANGE_START: "$routeChangeStart",
+    ROUTE_CHANGE_SUCCESS: "$routeChangeSuccess",
+    ROUTE_CHANGE_ERROR: "$routeChangeError"
+})
+/**
+ * Angular directive for a progress bar
+ * @example
+    <fm-loading-screen></fm-loading-screen>
+ * @class  fmLoadingScreen
+ * @module sn.fm.loadingScreen
+ * @author SOON_
+ */
+.directive("fmLoadingScreen",[
+    "$rootScope",
+    "EVENTS",
+    /**
+     * @constructor
+     * @param {Service} $rootScope
+     * @param {Object}  EVENTS
+     */
+    function ($rootScope, EVENTS){
+        return {
+            restrict: "EA",
+            templateUrl: "partials/loading.html",
+            link: function ($scope, $element){
+
+                $rootScope.$on(EVENTS.ROUTE_CHANGE_START, function(){
+                    $element.removeClass("hide");
+                });
+                $rootScope.$on(EVENTS.ROUTE_CHANGE_SUCCESS, function(){
+                    $element.addClass("hide");
+                });
+                $rootScope.$on(EVENTS.ROUTE_CHANGE_ERROR, function(){
+                    $element.addClass("hide");
+                });
+
+            }
+        };
+    }
+]);
