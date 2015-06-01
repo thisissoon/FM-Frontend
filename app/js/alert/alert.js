abaeeee9882346c12c28bd06f8939a7132197e8c
James Warren <mail@jamesjwarren.com>
2015-06-01 12:18:36 +0100

    split up module files and unit test

--- app/js/alert/alert.js
+++ app/js/alert/alert.js
@@ -4,80 +4,14 @@
  * @author   SOON_
  */
 angular.module("FM.alert", [
-    "ui.bootstrap.alert",
-    "ngAnimate"
+    "FM.alert.AlertCtrl",
+    "FM.alert.AlertService"
 ])
-/**
- * @class AlertService
- * @example
- *     AlertService.set("Woah there, you'll need to login", "warning");
- */
-.service("AlertService", [
+
+.run([
     "$rootScope",
-    function($rootScope){
-        var AlertService = {
-
-            /**
-             * Set new alert
-             * @method set
-             * @param {String} message  alert message to set
-             * @param {String} type     bootstrap style class (warning|danger|success|info)
-             */
-            set: function set (message, type) {
-                AlertService.alerts.push({
-                    message: message,
-                    type: type
-                });
-
-                $rootScope.alertActive = true;
-            },
-
-            /**
-             * Remove alert from list
-             * @method remove
-             * @param {Number} index   index of alert to remove
-             */
-            remove: function remove (index) {
-                AlertService.alerts.splice(index, 1);
-
-                if (AlertService.alerts.length === 0) {
-                    $rootScope.alertActive = false;
-                }
-            },
-
-            /**
-             * List of alerts
-             * @property {Array} alerts
-             */
-            alerts: []
-
-        };
-
-        return AlertService;
-    }
-])
-/**
- * @constructor
- * @class   AlertCtrl
- * @param   {Object} $scope
- */
-.controller("AlertCtrl", [
-    "$scope",
     "AlertService",
-    function($scope, AlertService){
-
-        /**
-         * List of alerts
-         * @property {Array} alerts
-         */
-        $scope.alerts = AlertService.alerts;
-
-        /**
-         * Close specific alert
-         * @method closeAlert
-         * @param {Number} index   of alert to close
-         */
-        $scope.closeAlert = AlertService.remove;
-
+    function ($rootScope, AlertService) {
+        $rootScope.AlertService = AlertService;
     }
 ]);
