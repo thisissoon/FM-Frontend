8b99af4212bedf3d9c2a6480f6646d65b1441c7e
James Warren <mail@jamesjwarren.com>
2015-06-05 15:31:34 +0100

    test and docs for snInfiniteScroll

--- app/js/infinite-scroll/infiniteScroll.js
+++ app/js/infinite-scroll/infiniteScroll.js
@@ -7,10 +7,14 @@ angular.module("sn.infiniteScroll", [
 
 ])
 /**
+ * Directive to trigger function when element reaches the bottom of its scroll
  * @example
-    <div sn-infinite-scroll="loadMore()" data-container="ng-view" data-offset="-100" data-disabled="loading">></div>
+ *   <div sn-infinite-scroll="loadMore()" data-container="ng-view" data-offset="-100" data-disabled="loading"></div>
  * @constructor
  * @class    snInfiniteScroll
+ * @require $window
+ * @require $document
+ * @require $timeout
  */
 .directive("snInfiniteScroll", [
     "$window",
@@ -21,33 +25,51 @@ angular.module("sn.infiniteScroll", [
             restrict: "A",
             scope: {
                 container: "@",
-                offset: "@",
+                offset: "=?",
                 snInfiniteScroll: "&",
-                disabled: "="
+                disabled: "=?"
             },
             link: function($scope, $element){
 
-                var scrollingElement = $scope.container ? $document[0].querySelector($scope.container) : $element,
-                    scrollOffset = $scope.offset || 0;
+                /**
+                 * Element to attach scroll listener to
+                 * Default to $element or lookup `container` identifier if defined
+                 * @property {Object} scrollElement
+                 */
+                var scrollElement = $scope.container ? $document[0].querySelector($scope.container) : $element;
 
+                /**
+                 * Offset from bottom of scroll to call trigger function (px)
+                 * @property {Number} offset
+                 */
+                $scope.offset = $scope.offset || 0;
+
+                /**
+                 * Determine if element has come to the end of its scroll and call the trigger function
+                 * @function onScroll
+                 */
                 var onScroll = function onScroll(){
 
-                    var elementScrollEnd = ( $window.innerHeight >= ($element[0].getBoundingClientRect().bottom - scrollOffset) );
+                    var elementScrollEnd = ( $window.innerHeight >= ($element[0].getBoundingClientRect().bottom - $scope.offset) );
 
                     if (elementScrollEnd && !$scope.disabled) {
                         $scope.snInfiniteScroll();
                     }
-
                 };
 
+                /**
+                 * Cleanup event listeners
+                 * @function removeListeners
+                 */
                 var removeListeners = function removeListeners(){
-                    angular.element(scrollingElement).off("scroll");
+                    angular.element(scrollElement).off("scroll");
                     angular.element($window).off("resize");
                 };
 
                 $scope.$on("$destroy", removeListeners);
 
-                angular.element(scrollingElement).on("scroll", onScroll);
+                // attach event listeners
+                angular.element(scrollElement).on("scroll", onScroll);
                 angular.element($window).on("resize", onScroll);
 
                 $timeout(onScroll, 500);
